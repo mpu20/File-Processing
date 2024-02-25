9efb18ed2b91ff8701c0dc18cf5e176672b22286
Nguyen Tran Minh (K17 HL) <minhnthe176001@fpt.edu.vn>
2024-02-25 19:16:43 +0700

    Change Local File System to Amazon S3

--- FileProcessing/Controllers/PlanController.cs
+++ FileProcessing/Controllers/PlanController.cs
@@ -1,54 +1,70 @@
+using Amazon.S3;
+using Amazon.S3.Model;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FileProcessing.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class PlanController : ControllerBase
+public class PlanController(IAmazonS3 s3Client, IConfiguration configuration) : ControllerBase
 {
-    [HttpGet]
-    [Route("download")]
-    public async Task<IActionResult> DownloadPlan()
+    private readonly IAmazonS3 _s3Client = s3Client;
+    private readonly IConfiguration _configuration = configuration;
+
+    [HttpGet("download")]
+    public async Task<IActionResult> DownloadPlan(string key)
     {
-        var filePath = @"Plan\plan.xlsx";
-        var memory = new MemoryStream();
-        await using (var stream = new FileStream(filePath, FileMode.Open))
+        var request = new GetObjectRequest
         {
-            await stream.CopyToAsync(memory);
+            BucketName = _configuration["AWS:BucketName"],
+            Key = key
+        };
+
+        using (var response = await _s3Client.GetObjectAsync(request))
+        using (var responseStream = response.ResponseStream)
+        {
+            MemoryStream memoryStream = new MemoryStream();
+            await responseStream.CopyToAsync(memoryStream);
+            memoryStream.Position = 0;
+
+            return File(memoryStream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", key);
         }
-
-        memory.Position = 0;
-
-        return File(memory, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-            Path.GetFileName(filePath));
     }
 
-    [HttpPost]
-    [Route("upload")]
+    [HttpPost("upload")]
     public async Task<IActionResult> UploadPlan(IFormFile? file)
     {
         if (file == null || file.Length == 0)
         {
             return BadRequest("File is empty");
         }
-        
+
         const int maxSize = 500 * 1024 * 1024; // 500MB
         if (file.Length > maxSize)
         {
             return BadRequest("File size exceeds 500MB");
         }
 
-        if (!Path.GetExtension(file.FileName).Equals(".xlsx", StringComparison.CurrentCultureIgnoreCase))
+        string[] validExtension = [".xls", ".xlsx", ".csv"];
+        if (!validExtension.Contains(Path.GetExtension(file.FileName).ToLower()))
         {
             return BadRequest("Invalid file format");
         }
 
-        var filePath = Path.Combine("Plan", file.FileName);
-        await using (var stream = new FileStream(filePath, FileMode.Create))
+        var key = file.FileName;
+        using (var stream = file.OpenReadStream())
         {
-            await file.CopyToAsync(stream);
+            var request = new PutObjectRequest
+            {
+                BucketName = _configuration["AWSCredentials:BucketName"],
+                Key = key,
+                InputStream = stream,
+                ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            };
+
+            await _s3Client.PutObjectAsync(request);
         }
 
-        return Ok("File uploaded successfully");
+        return Ok($"File {key} uploaded successfully!");
     }
 }
